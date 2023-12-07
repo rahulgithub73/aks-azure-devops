845b3c6eca2d24e988d9a28ac0ae1c1975d69cec
Rahul Kumar <rahul.kumar9@osttra.com>
2023-12-07 20:56:14 +0530

    code setup

---
+++ src/test/java/com/demo/aks/AKSDemo/AksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.aks.AKSDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
